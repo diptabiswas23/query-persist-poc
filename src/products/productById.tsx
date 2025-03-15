52c59e5e8668177d7adc01ed484b6432a5a9679e
diptabiswas23 <dipta.biswas@gyaan.ai>
2025-03-15 12:45:49 +0530

    updated

--- src/products/productById.tsx
+++ src/products/productById.tsx
@@ -1,5 +1,6 @@
 import { useQueryClient } from '@tanstack/react-query'
 import { getAllProducts } from './queries';
+import { memo } from 'react';
 
 const ProductById = () => {
 
@@ -8,17 +9,17 @@ const ProductById = () => {
     const query: any = queryClient.getQueryData(getAllProducts().queryKey)
 
 
-    console.log('ProductById -- Rendered', query.length, query)
+    console.log('ProductById -- Rendered', query.length)
 
 
   return (
     <div style={{marginLeft : '20px' , padding : "10px" , border: "1px solid red"}}>
         <p>Product By Id</p>
         <h6>Product Title</h6>
-      <p>{query?.[31]?.title}</p>
-      <span>{query?.[31]?.description}</span>
+      <p>{query?.[query.length - 1]?.title}</p>
+      <span>{query?.[query.length - 1]?.description}</span>
     </div>
   )
 }
 
-export default ProductById;
+export default memo(ProductById);
